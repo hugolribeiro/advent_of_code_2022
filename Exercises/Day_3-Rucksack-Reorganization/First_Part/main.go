c1ba4e3e3a020fe5ac3a0c334eed6d29d2864bcc
Hugo Ribeiro <hugolerib@gmail.com>
2022-12-04 21:13:50 -0300

    add day 3 exercise

---
+++ Exercises/Day_3-Rucksack-Reorganization/First_Part/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("../input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	fmt.Println(getSumPriorities(file))
+}
+
+func getSumPriorities(file *os.File) int {
+	scanner := bufio.NewScanner(file)
+
+	totalPriorityItems := 0
+
+	for scanner.Scan() {
+		rucksackContent := scanner.Text()
+		firstCompartment, secondCompartment := rucksackContent[0:len(rucksackContent)/2], rucksackContent[len(rucksackContent)/2:]
+		letterInCommon := getLetterInCommon(firstCompartment, secondCompartment)
+		itemPriority := getValueOfLetter(letterInCommon)
+
+		totalPriorityItems += itemPriority
+	}
+	return totalPriorityItems
+}
+
+func getLetterInCommon(firstString, secondString string) string {
+	letterInCommon := ""
+
+	for firstIdx := range firstString {
+		for secondIdx := range secondString {
+			if firstString[firstIdx] == secondString[secondIdx] {
+				letterInCommon = string(firstString[firstIdx])
+			}
+		}
+	}
+	return letterInCommon
+}
+
+func getValueOfLetter(letter string) int {
+	alphabet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	return strings.Index(alphabet, letter) + 1
+}
